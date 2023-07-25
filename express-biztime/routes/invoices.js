9b4dd7a1822d8c06a2fe06927d41d92ec8278615
Khoa Le <106197609+ArritakiItto@users.noreply.github.com>
2023-07-25 00:03:52 -0700

    Update invoices.js

--- express-biztime/routes/invoices.js
+++ express-biztime/routes/invoices.js
@@ -13,26 +13,28 @@ router.get('/', async (req, res, next) => {
 });
 
 //GET/invoices/[id]
-router.get('/:id', async (req, res, next) => {
+router.get("/:id", async function (req, res, next) {
   try {
-    const  id  = req.params;
+    let id = req.params.id;
+
     const result = await db.query(
-      `SELECT i.id, 
-              i.comp_code, 
-              i.amt, 
-              i.paid, 
-              i.add_date, 
-              i.paid_date, 
-              c.name, 
-              c.description 
-       FROM invoices AS i
-         INNER JOIN companies AS c ON (i.comp_code = c.code)  
-       WHERE id = $1`,
-      [id]
-    );
+          `SELECT i.id, 
+                  i.comp_code, 
+                  i.amt, 
+                  i.paid, 
+                  i.add_date, 
+                  i.paid_date, 
+                  c.name, 
+                  c.description 
+           FROM invoices AS i
+             INNER JOIN companies AS c ON (i.comp_code = c.code)  
+           WHERE id = $1`,
+        [id]);
+
     if (result.rows.length === 0) {
-      throw new ExpressError(`No such invoice: ${id}`, 404);
+      throw new ExpressError(`No such invoice: ${id}`,404);
     }
+
     const data = result.rows[0];
     const invoice = {
       id: data.id,
@@ -46,67 +48,27 @@ router.get('/:id', async (req, res, next) => {
       add_date: data.add_date,
       paid_date: data.paid_date,
     };
-    return res.json({ invoice: invoice });
-  } catch (err) {
+
+    return res.json({"invoice": invoice});
+  }
+
+  catch (err) {
     return next(err);
   }
 });
 
 //POST/invoices
-router.post('/', async (req, res, next) => {
+router.post("/", async function (req, res, next) {
   try {
-    const { comp_code, amt } = req.body;
-    const result = await db.query(
-      'INSERT INTO invoices (comp_code, amt) VALUES ($1, $2) RETURNING id, comp_code, amt, paid, add_date, paid_date',
-      [comp_code, amt]
-    );
-    return res.status(201).json({ invoice: result.rows[0] });
-  } catch (err) {
-    return next(err);
-  }
-});
-
-
-//PUT/invoices/[id]
-router.put("/:id", async function (req, res, next) {
-  try {
-    let { amt, paid } = req.body;
-    let id = req.params.id;
-    let paid_date = null;
-
-    const oldInvoice = await db.query(
-          `SELECT paid
-           FROM invoices
-           WHERE id = $1`,
-        [id]
-    );
-
-    if (oldInvoice.rows.length === 0) {
-      throw new ExpressError(`No such invoice: ${id}`, 404)
-    }
-
-    const oldPaidDate = oldInvoice.rows[0].paid_date;
-
-    if (!oldPaidDate && paid) {
-      paid_date = new Date();
-    } else if (!paid) {
-      paid_date = null
-    } else {
-      paid_date = oldPaidDate;
-    }
+    let {comp_code, amt} = req.body;
 
     const result = await db.query(
-          `UPDATE invoices
-           SET amt=$1, paid=$2, paid_date=$3
-           WHERE id = $4
+          `INSERT INTO invoices (comp_code, amt) 
+           VALUES ($1, $2) 
            RETURNING id, comp_code, amt, paid, add_date, paid_date`,
-        [amt, paid, paid_date, id]);
+        [comp_code, amt]);
 
-    if (result.rows.length === 0) {
-      throw new ExpressError(`No such invoice: ${id}`, 404)
-    } else {
-      return res.json({"invoice": result.rows[0]});
-    }
+    return res.json({"invoice": result.rows[0]});
   }
 
   catch (err) {
@@ -115,19 +77,74 @@ router.put("/:id", async function (req, res, next) {
 });
 
 
-//DELETE/invoices/[id]
-router.delete('/:id', async (req, res, next) => {
+//PUT/invoices/[id]
+router.put("/:id", async function (req, res, next) {
   try {
-    const { id } = req.params;
-    const result = await db.query('DELETE FROM invoices WHERE id = $1 RETURNING id', [id]);
+    let {amt, paid} = req.body;
+    let id = req.params.id;
+    let paidDate = null;
+
+    const currResult = await db.query(
+          `SELECT paid
+           FROM invoices
+           WHERE id = $1`,
+        [id]);
+
+    if (currResult.rows.length === 0) {
+      throw new ExpressError(`No such invoice: ${id}`, 404);
+    }
+
+    const currPaidDate = currResult.rows[0].paid_date;
+
+    if (!currPaidDate && paid) {
+      paidDate = new Date();
+    } else if (!paid) {
+      paidDate = null
+    } else {
+      paidDate = currPaidDate;
+    }
+
+    const result = await db.query(
+          `UPDATE invoices
+           SET amt=$1, paid=$2, paid_date=$3
+           WHERE id=$4
+           RETURNING id, comp_code, amt, paid, add_date, paid_date`,
+        [amt, paid, paidDate, id]);
+
+    return res.json({"invoice": result.rows[0]});
+  }
+
+  catch (err) {
+    return next(err);
+  }
+
+});
+
+
+
+//DELETE/invoices/[id]
+router.delete("/:id", async function (req, res, next) {
+  try {
+    let id = req.params.id;
+
+    const result = await db.query(
+          `DELETE FROM invoices
+           WHERE id = $1
+           RETURNING id`,
+        [id]);
+
     if (result.rows.length === 0) {
       throw new ExpressError(`No such invoice: ${id}`, 404);
     }
-    return res.json({ status: 'deleted' });
-  } catch (err) {
+
+    return res.json({"status": "deleted"});
+  }
+
+  catch (err) {
     return next(err);
   }
 });
+
 
 module.exports = router;
 
